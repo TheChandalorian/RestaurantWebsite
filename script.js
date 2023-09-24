2e8c6b5ee445624c09339e5c8f8b1e2421d7e314
Cjft92 <100280939+TheChandalorian@users.noreply.github.com>
2023-09-24 11:45:37 +0100

    Create script.js
    js for loading page/once loaded

---
+++ script.js
@@ -0,0 +1,11 @@
+/** Proload 
+ * 
+ * Loading will be end after document is loaded
+*/
+
+const preloader = document.querySelector("[data-preload]");
+
+window.addEventListener("load", function () {
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+});
