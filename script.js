e35d7adb5f16926000debda6e6b797c9f3845c14
Cjft92 <100280939+TheChandalorian@users.noreply.github.com>
2023-09-29 08:32:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 */
 
 const preloader = document.querySelector("[data-preload]");
-
+ 
 window.addEventListener("load", function () {
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
